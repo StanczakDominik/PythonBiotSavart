2b9d5e040bbbfa3200eb87006000f6c0d3504156
Dominik Stańczak <stanczakdominik@gmail.com>
2015-08-11 09:13:10 +0200

    uniform grid

--- biot.py
+++ biot.py
@@ -26,9 +26,18 @@ if(os.path.isfile(folder_name+"grid_positions.dat")):
     grid_positions=np.loadtxt(folder_name+"grid_positions.dat")
     print("Loaded grid positions")
 else:
-    x,dx=np.linspace(xmin,xmax,NGRID,retstep=True)
-    y,dy=np.linspace(ymin,ymax,NGRID,retstep=True)
-    z,dz=np.linspace(zmin,zmax,NZGRID,retstep=True)
+    dx = dy = (ymax - ymin)/NGRID
+    dz = (zmax-zmin)/NZGRID
+    step_size=min([dx, dz])
+
+    x = y = np.arange(xmin, xmax, step_size)
+    z = np.arange(zmin, zmax, step_size)
+
+    NGRID = len(x)
+    NZGRID = len(z)
+    # x,dx=np.linspace(xmin,xmax,NGRID,retstep=True)
+    # y,dy=np.linspace(ymin,ymax,NGRID,retstep=True)
+    # z,dz=np.linspace(zmin,zmax,NZGRID,retstep=True)
 
     grid_positions=np.zeros((NGRID**2*NZGRID,3))
     for ix, vx in enumerate(x):
@@ -87,30 +96,30 @@ dt = dt_cyclotron
 mytree = scipy.spatial.cKDTree(grid_positions)
 
 def calculate_field(r):
-    # distances, indices = mytree.query(r, k=25)
-    # weights =1./(distances)
-    # sum_weights=np.sum(weights)
-    # local_B=grid_B[indices]
-    #
-    # interpolated_BX = np.sum(local_B[:,0]*weights)/sum_weights
-    # interpolated_BY = np.sum(local_B[:,1]*weights)/sum_weights
-    # interpolated_BZ = np.sum(local_B[:,2]*weights)/sum_weights
-    # array = np.array([interpolated_BX,interpolated_BY,interpolated_BZ])
-    # return array
-    B=np.zeros(3)
-    B0 = MU*wire_current/5.*np.pi
-    distances = np.sqrt(np.sum(r[:2]**2))
-    orientation=r/distances
+    distances, indices = mytree.query(r, k=25)
+    weights =1./(distances)
+    sum_weights=np.sum(weights)
+    local_B=grid_B[indices]
 
-    if distances<r_wires:
-        B[0] = B0 * distances/r_wires*orientation[1]
-        B[1] = B0 * distances/r_wires*orientation[0]*(-1)
-    else:
-        B[0] = B0 * r_wires / distances*orientation[1]
-        B[1] = B0 * r_wires / distances*orientation[0]*(-1)
-    B[np.isinf(B)] = 0
-    B[np.isnan(B)] = 0
-    return B
+    interpolated_BX = np.sum(local_B[:,0]*weights)/sum_weights
+    interpolated_BY = np.sum(local_B[:,1]*weights)/sum_weights
+    interpolated_BZ = np.sum(local_B[:,2]*weights)/sum_weights
+    array = np.array([interpolated_BX,interpolated_BY,interpolated_BZ])
+    return array
+    # B=np.zeros(3)
+    # B0 = MU*wire_current/5.*np.pi
+    # distances = np.sqrt(np.sum(r[:2]**2))
+    # orientation=r/distances
+    #
+    # if distances<r_wires:
+    #     B[0] = B0 * distances/r_wires*orientation[1]
+    #     B[1] = B0 * distances/r_wires*orientation[0]*(-1)
+    # else:
+    #     B[0] = B0 * r_wires / distances*orientation[1]
+    #     B[1] = B0 * r_wires / distances*orientation[0]*(-1)
+    # B[np.isinf(B)] = 0
+    # B[np.isnan(B)] = 0
+    # return B
 
 def boris_step(r, v, dt):
     field = calculate_field(r)
